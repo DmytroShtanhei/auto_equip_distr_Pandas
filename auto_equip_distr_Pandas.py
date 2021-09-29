0408f70d7d994300ad28e48d58607315ac43b8cf
Dmytro Shtanhei <shtanhei@gmail.com>
2021-09-29 10:21:38 +0300

    Initial information into one xlsx file

--- auto_equip_distr_Pandas.py
+++ auto_equip_distr_Pandas.py
@@ -10,13 +10,13 @@ import datetime
 
 pd.set_option("expand_frame_repr", False)
 
-df_grouping = pd.read_excel("Групування.xlsx", usecols="D, H, M")  # optional parameter: sheet_name="Лист1"
+df_grouping = pd.read_excel("Групування-КодиЛВУ-Позиції.xlsx", sheet_name="Групування", usecols="D, H, M")
 
 # Fill out missing values for "Поз. в Договорі"
 df_grouping = df_grouping.fillna(method="pad")
 
 # Add information about Warehouses
-df_lvu_warehouses = pd.read_excel("ЛВУ - коди-назви.xlsx", usecols="A, E")
+df_lvu_warehouses = pd.read_excel("Групування-КодиЛВУ-Позиції.xlsx", sheet_name="КодиЛВУ", usecols="A, E")
 df_grouping = pd.merge(df_grouping, df_lvu_warehouses, how="left", on="Код Заводу")
 
 # Group rows by common criteria and calculate sums for each group
@@ -28,7 +28,7 @@ df_grouping = df_grouping.unstack("Код Заводу", fill_value=0)
 
 # Rename columns names
 # Read codes and names columns
-df_lvu_names = pd.read_excel("ЛВУ - коди-назви.xlsx", usecols="A, C")
+df_lvu_names = pd.read_excel("Групування-КодиЛВУ-Позиції.xlsx", sheet_name="КодиЛВУ", usecols="A, C")
 # Convert Dataframe to Series
 s_lvu_names = pd.Series(df_lvu_names["Актуальна назва підрозділу"].values, index=df_lvu_names["Код Заводу"])
 # Rename codes to names in column names
@@ -39,7 +39,7 @@ df_grouping.reset_index(inplace=True)
 df_grouping.columns = [' '.join(col).strip("Кільк.").strip() for col in df_grouping.columns.values]
 
 # Add more info about Warehouses (at the end of DF)
-df_warehouses_info = pd.read_excel("ЛВУ - коди-назви.xlsx", usecols="E:H")
+df_warehouses_info = pd.read_excel("Групування-КодиЛВУ-Позиції.xlsx", sheet_name="КодиЛВУ", usecols="E:H")
 df_warehouses_info.drop_duplicates(inplace=True)
 df_grouping = pd.merge(df_grouping, df_warehouses_info, how="left", on="Завод зберігання")
 # Move last three columns with info about Warehouses to second position
@@ -48,7 +48,7 @@ df_grouping = pd.concat(
     axis=1)
 
 # Add more info about Products
-df_contract = pd.read_excel("Склад.xlsx", usecols="A:C, E:F")
+df_contract = pd.read_excel("Групування-КодиЛВУ-Позиції.xlsx", sheet_name="Позиції", usecols="A:C, E:F")
 df_grouping = pd.merge(df_grouping, df_contract, how="left", on="Поз. в Договорі")
 # Move last four columns with info about Products to fifth position
 df_grouping = pd.concat(
